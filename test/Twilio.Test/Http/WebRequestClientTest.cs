d7f3514a1a784955dc4b2f200f9257ddfd67cfbf
Evan Fossier <evan.fossier@gmail.com>
2017-08-17 13:30:55 -0700

    WIP Start testing WebRequestClient

---
+++ test/Twilio.Test/Http/WebRequestClientTest.cs
@@ -0,0 +1,17 @@
+﻿#if NET35
+using NUnit.Framework;
+
+namespace Twilio.Tests.Http
+{
+    [TestFixture]
+    public class WebRequestClientTest : TwilioTest
+    {
+        [Test]
+        public void TestMakeRequestSuccess()
+        {
+            Assert.AreEqual(true, false);
+
+        }
+    }
+}
+#endif
